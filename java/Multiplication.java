2db535bc64d722593407d82410cbb67c56b8d672
Ernest <musaubrian15@gmail.com>
2023-01-26 14:34:39 +0300

    fix: capitalize class

--- java/Multiplication.java
+++ java/Multiplication.java
@@ -1,4 +1,4 @@
-public class multiplication {
+public class Multiplication {
     public static void main(String[] args) {
         for (int i = 0; i <= 9; i++) {
             for (int j = 0; j <= 9; j++) {
