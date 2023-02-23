255d51083cd09b924d61e133a55bb113d6e1d1dd
Ernest <musaubrian15@gmail.com>
2023-02-23 07:24:52 +0300

    chore: add check buttons
    in their different *states
    disabled, enabled and unchecked

--- python/idle.py
+++ python/idle.py
@@ -3,13 +3,15 @@ from tkinter import ttk
 
 win = tk.Tk()
 win.title('python GUI')
-win.resizable(0,0)
+win.resizable(5,5)
 
 a_label = ttk.Label(win, text='Enter your name:')
 a_label.grid(column=0, row=0)
+
 # Set the textbox
 name = tk.Entry()
 name.grid(column=0, row=1)
+name.focus()
 
 #Set the drobdown menu
 ttk.Label(win, text="Choose a number: ").grid(column=1, row=0)
@@ -29,4 +31,24 @@ def click_me():
 
 action = ttk.Button(win, text="Click me", command=click_me)
 action.grid(column=2, row=1)
+
+# 3 buttons
+ch_var_dis = tk.IntVar()
+
+# disabled check button
+check_1=tk.Checkbutton(win, text="Disabled", variable=ch_var_dis, state="disabled")
+check_1.select()
+check_1.grid(column=0, row=4, sticky=tk.W)
+
+#Unchecked check button
+ch_var_un = tk.IntVar()
+check_2 = tk.Checkbutton(win, text="unChecked", variable=ch_var_un)
+check_2.deselect()
+check_2.grid(column=1, row=4, sticky=tk.W)
+
+ch_var_en = tk.IntVar()
+check_3 = tk.Checkbutton(win, text="Enabled", variable=ch_var_en)
+check_3.select()
+check_3.grid(column=2, row=4, sticky=tk.W)
+
 win.mainloop()
